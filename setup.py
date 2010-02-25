7c315e5ec37e696b53358f6c1c593c56d32660e2
egutesman <egutesman@64623a4a-1b68-11df-8372-0b5b75a1557e>
2010-02-25 19:14:36 +0000

    minor changes

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup, Extension
 
-pylorcon = Extension('PyLorcon2',
-		sources = ['PyLorcon2.c'],
-		libraries = ['orcon2', 'pcap', 'nl'])
+PyLorcon2 = Extension('PyLorcon2',
+                      sources = ['PyLorcon2.c'],
+                      libraries = ['orcon2', 'pcap', 'nl'])
 
 setup(name = 'PyLorcon2',
-		version = '0.1',
-		description = 'Python wrapper for the Lorcon2 library',
-    license = 'GNU GPLv3',
-		author = 'Andres Blanco (6e726d), Ezequiel Gutesman (gutes)',
-		author_email = '6e726d@gmail.com, egutesman@coresecurity.com',
-    url = 'http://code.google.com/p/pylorcon2',
-		ext_modules = [PyLorcon2])
+      version = '0.1',
+      description = 'Python wrapper for the Lorcon2 library',
+      license = 'GNU GPLv3',
+      author = 'Andres Blanco (6e726d), Ezequiel Gutesman (gutes)',
+      author_email = '6e726d@gmail.com, egutesman@coresecurity.com',
+      url = 'http://code.google.com/p/pylorcon2',
+      ext_modules = [PyLorcon2])
