0f116df2620b286e3ede7ec884cba25abddcd58f
egutesman <egutesman@64623a4a-1b68-11df-8372-0b5b75a1557e>
2010-02-25 15:18:39 +0000

    License & setup.py added.

--- PyLorcon2.c
+++ PyLorcon2.c
@@ -1,8 +1,25 @@
 /*
- * PyLorcon2 - Python bindings for Lorcon2 library
- * Author: 6e726d <6e726d@gmail.com>
- * Author: gutes  <egutesman@coresecurity.com>
- */
+    PyLorcon2 - Python bindings for Lorcon2 library
+    Copyright (C) 2010  Core Security Technologies
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+    Author: Andres Blanco (6e726d)     <6e726d@gmail.com>
+    Author: Ezequiel Gutesman (gutes)  <egutesman@coresecurity.com>
+*/
 
 #include "PyLorcon2.h"
 
